7db8499a37d0149d2dbf6af8aab906993d919856
susanacorrea <32500480+susanacorrea@users.noreply.github.com>
2018-06-03 21:07:48 -0500

    Add files via upload

---
+++ ruby-on-rails/quiz/app/models/question.rb
@@ -0,0 +1,25 @@
+class Question < ApplicationRecord
+  has_many :choices
+
+  def uncorrect
+    choices.each {|c| c.correct = false}
+  end
+
+  def answer
+    uncorrect
+    choices.select {|c| c.correct}[0]
+  end
+
+  def answer= choice
+    if !answer.nil?
+      answer.correct = false
+    end
+    if choices.include? choice
+      choice.correct = true
+    else
+      choices << choice
+      choice.correct = true
+    end
+  end
+
+end
